c5f501baa478e699d0530cb7cffc683243f7618b
Sean Taitano <dumberdrummer@gmail.com>
2020-09-05 23:52:27 -0700

    Exercise 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -12,19 +12,19 @@ const Total = (props) => {
     <p> Number of Exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   )
 }
-
+const CoursePart = (props) =>{
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
 const Content = (props) =>{
   return (
     <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
+<CoursePart part = {props.part1} exercises = {props.exercises1} />
+<CoursePart part = {props.part2} exercises = {props.exercises2} />
+<CoursePart part = {props.part3} exercises = {props.exercises3} />
     </>
   )
 }
